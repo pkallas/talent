85df6f9be3a72207dc580df9ec0873740727ea53
Jonathan Lubaway <NeoJax@gmail.com>
2018-02-06 14:28:29 -0800

    Fixed the navbar for 375 px mobile viewing and normal laptop viewing

--- src/client/containers/profile/profile/index.jsx
+++ src/client/containers/profile/profile/index.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap';
 import Blurb from '../../../components/blurb';
 
 export default class Profile extends Component {
@@ -11,13 +10,13 @@ export default class Profile extends Component {
           <img className="img-responsive col-sm-5" src="/LearnerImage.png" />
           <Blurb className="col-sm-4" info={this.props.info} />
         </div>
-        <ButtonToolbar className="row">
-          <DropdownButton bsSize="large" title="Personal Contact Information" id="dropdown-size-large">
-            <MenuItem href={`https://github.com/${this.props.github_handle}`} target="_blank" eventKey="1">Github</MenuItem>
-            <MenuItem href={`https://www.linkedin.com/in/${this.props.linkedin_profile}`} target="_blank" eventKey="2">Linkedin</MenuItem>
-            <MenuItem href={`https://www.twitter.com/${this.props.twitter}`} target="_blank" eventKey="3">Twitter</MenuItem>
-          </DropdownButton>
-        </ButtonToolbar>
+        <div className="row">
+          <div bsSize="large" title="Personal Contact Information" id="dropdown-size-large">
+            <div href={`https://github.com/${this.props.github_handle}`} target="_blank" eventKey="1">Github</div>
+            <div href={`https://www.linkedin.com/in/${this.props.linkedin_profile}`} target="_blank" eventKey="2">Linkedin</div>
+            <div href={`https://www.twitter.com/${this.props.twitter}`} target="_blank" eventKey="3">Twitter</div>
+          </div>
+        </div>
       </div>
     );
   }
